49f6f0f5826b9082b2a6b470c46541af0ab111db
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-03-18 12:34:53 +0000

    added checker and getRatings

--- src/dataOut/usersXtv.js
+++ src/dataOut/usersXtv.js
@@ -27,3 +27,28 @@ export async function deleteState() {
 export async function getStateAndRating() {
   
 }
+
+export async function checkRatingBySeriesId(id) {
+  const q = 'SELECT * FROM users WHERE series = $1';
+  let result = '';
+  try {
+    result = await query(q, [id]);
+  } catch (e) {
+    console.info('Error occured :>> ', e);
+  }
+  if (result.rowCount >= 1) {
+    return true;
+  }
+  return false;
+}
+
+export async function getRatingBySeriesId(id) {
+  const q = 'SELECT * FROM users WHERE series = $1';
+  let result = '';
+  try {
+    result = await query(q, [id]);
+  } catch (e) {
+    console.info('Error occured :>> ', e);
+  }
+  return result.rows;
+}
