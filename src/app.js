df648a651a7f9d974bdcd733267dc49319f3f2cf
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-03-18 13:20:27 +0000

    added menu for method of each href

--- src/app.js
+++ src/app.js
@@ -37,6 +37,118 @@ app.use(session({
   maxAge: 20 * 1000, // 20 sek
 }));
 
+app.get('/', async (req, res) => {
+  res.json({
+    tv: {
+      series: {
+        href: '/tv',
+        methods: [
+          'GET',
+          'POST',
+        ],
+      },
+      serie: {
+        href: '/tv/{id}',
+        methods: [
+          'GET',
+          'PATCH',
+          'DELETE',
+        ],
+      },
+      rate: {
+        href: '/tv/{id}/rate',
+        methods: [
+          'POST',
+          'PATCH',
+          'DELETE',
+        ],
+      },
+      state: {
+        href: '/tv/{id}/state',
+        methods: [
+          'POST',
+          'PATCH',
+          'DELETE',
+        ],
+      },
+    },
+    seasons: {
+      seasons: {
+        href: '/tv/{id}/season',
+        methods: [
+          'GET',
+          'POST',
+        ],
+      },
+      season: {
+        href: '/tv/{id}/season/{season}',
+        methods: [
+          'GET',
+          'DELETE',
+        ],
+      },
+    },
+    episodes: {
+      episodes: {
+        href: '/tv/{id}/season/{season}/episode',
+        methods: [
+          'POST',
+        ],
+      },
+      episode: {
+        href: '/tv/{id}/season/{season}/episode/{episode}',
+        methods: [
+          'GET',
+          'DELETE',
+        ],
+      },
+    },
+    genres: {
+      genres: {
+        href: '/genres',
+        methods: [
+          'GET',
+          'POST',
+        ],
+      },
+    },
+    users: {
+      users: {
+        href: '/users',
+        methods: [
+          'GET',
+        ],
+      },
+      user: {
+        href: '/users/{id}',
+        methods: [
+          'GET',
+          'PATCH',
+        ],
+      },
+      register: {
+        href: '/users/register',
+        methods: [
+          'POST',
+        ],
+      },
+      login: {
+        href: '/users/login',
+        methods: [
+          'POST',
+        ],
+      },
+      me: {
+        href: '/users/me',
+        methods: [
+          'GET',
+          'PATCH',
+        ],
+      },
+    },
+  });
+});
+
 app.use(routerUsers);
 app.use(routerTV);
 app.use(routerUserXtv);
