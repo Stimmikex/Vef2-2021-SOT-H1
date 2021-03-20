bfbc5a4e15d95bdc608940308950c9bcb7ebb5ec
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-03-20 18:04:52 +0000

    added some userXtv.js validation and sans

--- src/routes/usersXtv.js
+++ src/routes/usersXtv.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import xss from 'xss';
+import {
+  body,
+  params,
+  validationResult,
+} from 'express-validator';
 import {
   addRating,
   addState,
@@ -15,33 +21,58 @@ export const routerUserXtv = express.Router();
 /**
  * /tv/:data?/rate
  */
-routerUserXtv.post('/tv/:seriesId?/rate', requireAuthentication, async (req, res) => {
-  const data = req.body;
-  const { seriesId } = req.params;
-  await addRating(data.rating, seriesId, req.user.id);
-  console.info('Rating has benn added');
-});
+routerUserXtv.post('/tv/:seriesId?/rate',
+  body('rating')
+    .isNumeric({ min: 0, max: 5 }),
+  requireAuthentication, async (req, res) => {
+    const { rating } = req.body;
+    const xssRating = xss(rating);
+    const { seriesId } = req.params;
+    const xssSeriesId = xss(seriesId);
+    await addRating(xssRating, xssSeriesId, req.user.id);
+    res.json({
+      msg: 'Rating has been added',
+      rating: xssRating,
+      seriesId: xssSeriesId,
+    });
+  });
 
-routerUserXtv.patch('/tv/:data?/rate', requireAuthentication, async (req, res) => {
-  const data = req.body;
-  const { seriesId } = req.params;
-  await updateRating(data.rating, seriesId, req.user.id);
-  console.info('update Rating');
-});
+routerUserXtv.patch('/tv/:data?/rate',
+  body('rating')
+    .isNumeric({ min: 0, max: 5 }),
+  requireAuthentication, async (req, res) => {
+    const { rating } = req.body;
+    const xssRating = xss(rating);
+    const { seriesId } = req.params;
+    const xssSeriesId = xss(seriesId);
+    await updateRating(xssRating, xssSeriesId, req.user.id);
+    res.json({
+      msg: 'update Rating',
+      rating: xssRating,
+      seriesId: xssSeriesId,
+    });
+  });
 
-routerUserXtv.delete('/tv/:data?/rate', requireAuthentication, async (req, res) => {
-  const { seriesId } = req.params;
-  deleteRating(seriesId, req.user.id);
-  console.info('delete Rating');
-});
+routerUserXtv.delete('/tv/:data?/rate',
+  params('seriesId')
+    .isInt(),
+  requireAuthentication, async (req, res) => {
+    const { seriesId } = req.params;
+    const xssSeriesId = xss(seriesId);
+    deleteRating(xssSeriesId, req.user.id);
+    res.json({
+      msg: 'delete Rating',
+      seriesId: xssSeriesId,
+    });
+  });
 
 /**
  * /tv/:data?/state
  */
 routerUserXtv.post('/tv/:data?/state', requireAuthentication, async (req, res) => {
-  const data = req.body;
+  const { rating } = req.body;
   const { seriesId } = req.params;
-  addState(data.rating, seriesId, req.user.id);
+  addState(rating, seriesId, req.user.id);
   console.info('state added');
 });
 
